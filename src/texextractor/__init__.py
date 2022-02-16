49c698411ddb0af9fd8ac264dc82fdc8cf29e1bc
BZoennchen <benedikt.zoennchen@web.de>
2022-02-16 23:26:52 +0100

    refactoring and testing.

--- src/texextractor/__init__.py
+++ src/texextractor/__init__.py
@@ -8,7 +8,7 @@ __author__ = 'Benedikt Zoennchen'
 import re
 
 
-def inner_extract(text, obracket, cbracket):
+def __extract_within_brackets(text, obracket, cbracket):
     pos = text.find(obracket)
     startpos = pos
     count = 0
@@ -21,7 +21,7 @@ def inner_extract(text, obracket, cbracket):
         else:
             break
 
-    for i in range(count):
+    for _ in range(count):
         pos = text.find(cbracket, pos+1)
         if pos < 0:
             raise Exception('incorrect formated string!')
@@ -29,84 +29,92 @@ def inner_extract(text, obracket, cbracket):
     return (startpos, endpos)
 
 
-def extract(paperFileName):
-    data = open(paperFileName).read()
-    pos = data.find("\\author")
-    shortauthors = ""
-    longauthors = ""
-    shorttitle = ""
-    longtitle = ""
-    abstract = ""
-    keywords = ""
-
+def extract_bracket(text, lead, obracket, cbracket):
+    pos = text.find(lead)
+    extraction = ""
     if pos > -1:
-        data = data[pos + len("\\author"):]
-        startpos, endpos = inner_extract(data, '[', ']')
+        text = text[pos + len(lead):]
+        startpos, endpos = __extract_within_brackets(text, obracket, cbracket)
         if startpos >= 0 and endpos >= 0:
-            shortauthors = data[startpos+1:endpos]
+            extraction = text[startpos+1:endpos]
+    return extraction
 
-        startpos, endpos = inner_extract(data, '{', '}')
-        if startpos >= 0 and endpos >= 0:
-            longauthors = data[startpos+1:endpos]
 
-    data = open(paperFileName).read()
-    pos = data.find(r"\title")
+def extract_optional(text, lead):
+    return extract_bracket(text, lead, '[', ']')
 
+
+def extract_non_optional(text, lead):
+    return extract_bracket(text, lead, '{', '}')
+
+
+def extract_shortauthors(text):
+    return extract_optional(text, "\\author")
+
+
+def extract_authors(text):
+    return extract_non_optional(text, "\\author")
+
+
+def extract_shorttitle(text):
+    return extract_optional(text, "\\title")
+
+
+def extract_title(text):
+    return extract_non_optional(text, "\\title")
+
+
+def extract_env(text, env):
+    env_content = ""
+    pos = text.find("\\begin{" + env + "}")
     if pos > -1:
-        data = data[pos + len(r"\title"):].strip()
+        text = text[pos + len("\\begin{" + env + "}")):]
+        endpos=text.find("\\end{"+env+"}")
+        env_content=text[0:endpos].strip()
+    return env_content
 
-        startpos, endpos = inner_extract(data, '[', ']')
-        if startpos >= 0 and endpos >= 0:
-            shorttitle = clear_title(data[startpos+1:endpos], True)
-
-        startpos, endpos = inner_extract(data, '{', '}')
-        if startpos >= 0 and endpos >= 0:
-            longtitle = clear_title(data[startpos+1:endpos])
-    else:
-        print(paperFileName)
-
-    data = open(paperFileName).read()
-    pos = data.find("\\begin{abstract}")
-    if pos > -1:
-        data = data[pos + len("\\begin{abstract}"):]
-        endpos = data.find("\\end{abstract}")
-        abstract = data[0:endpos].strip()
-
-    data = open(paperFileName).read()
-    pos = data.find("\\begin{keywords}")
-    if pos > -1:
-        data = data[pos + len("\\begin{keywords}"):]
-        endpos = data.find("\\end{keywords}")
-        keywords = data[0:endpos].strip()
-
+def extract(paper_file_name):
+    text=open(paper_file_name).read()
+    shortauthors=extract_shortauthors(text)
+    longauthors=extract_authors(text)
+    shorttitle=extract_shorttitle(text)
+    longtitle=extract_title(text)
+    abstract=extract_env(text, "abstract")
+    keywords=extract_env(text, "keywords")
     return shortauthors, longauthors, shorttitle, longtitle, abstract, keywords
 
-
 def remove_comments(tex_text):
-    out = ''
+    out=''
     print(tex_text.splitlines())
     for line in tex_text.splitlines():
-        new_line = line
-        pos = line.find("%")
+        new_line=line
+        pos=line.find("%")
         if pos > -1:
-            new_line = line[:pos]
+            new_line=line[:pos]
         out += new_line + '\n'
     return out.strip()
 
-
-def clear_title(title, is_short_title=False):
-    title = title.replace("%", "")
-    title = title.replace(r"\\", "")
-    title = title.replace("\n", "")
-    title = title.replace("\break", "")
-    title = title.replace("\centering", "")
-    title = re.sub(r"\s+", " ", title)
-    title = re.sub(r"\[.*?\] *", "", title)
-    title = re.sub(
+def clear_title(title, is_short_title = False):
+    title=title.replace("%", "")
+    title=title.replace(r"\\", "")
+    title=title.replace("\n", "")
+    title=title.replace("\break", "")
+    title=title.replace("\centering", "")
+    title=re.sub(r"\s+", " ", title)
+    title=re.sub(r"\[.*?\] *", "", title)
+    title=re.sub(
         r"\\(textnormal|vspace|small|large){.*?}", "", title)
-    title = re.sub(r"\\(systemname){.*?} :", "", title)
+    title=re.sub(r"\\(systemname){.*?} :", "", title)
     if is_short_title:
-        title = re.sub(r"---.*$", "", title)
-    title = re.sub(r"\\large.*$", "", title)
-    title = title.strip()
+        title=re.sub(r"---.*$", "", title)
+    title=re.sub(r"\\large.*$", "", title)
+    title=title.strip()
     return title
+
+def texify(text):
+    tex_replacement_map={'é': "\\'e", 'ó': "\\'{o}", 'ć': "\\'{c}", 'Ä': '\\"A', 'Ö': '\\"O', 'Ü': '\\"U',
+                           'ß': '{\\ss}', 'ä': '\\"a', 'ö': '\\"o', 'ü': '\\"u', 'ñ': '\\~{n}', 'ń': "\\'n",
+                           '„': '\\glqq', '“': '\\grqq', '”': "''", '’': "'{}", ' - ': ' --- '}
+    for char in tex_replacement_map:
+        text = text.replace(char, tex_replacement_map[char])
+    return text
---
+++ src/texextractor/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# coding: utf-8
+
+import texextractor as te
+
+if __name__ == "__main__":
+    print(te.extract("./data/paper.tex"))
