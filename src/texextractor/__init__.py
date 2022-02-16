977dc64215faf9266214f4758871298316dd810a
BZoennchen <benedikt.zoennchen@web.de>
2022-02-17 00:20:26 +0100

    refactoring and testing.

--- src/texextractor/__init__.py
+++ src/texextractor/__init__.py
@@ -68,51 +68,55 @@ def extract_env(text, env):
     env_content = ""
     pos = text.find("\\begin{" + env + "}")
     if pos > -1:
-        text = text[pos + len("\\begin{" + env + "}")):]
-        endpos=text.find("\\end{"+env+"}")
-        env_content=text[0:endpos].strip()
+        text = text[pos + len("\\begin{" + env + "}"):]
+        endpos = text.find("\\end{"+env+"}")
+        env_content = text[0:endpos].strip()
     return env_content
 
+
 def extract(paper_file_name):
-    text=open(paper_file_name).read()
-    shortauthors=extract_shortauthors(text)
-    longauthors=extract_authors(text)
-    shorttitle=extract_shorttitle(text)
-    longtitle=extract_title(text)
-    abstract=extract_env(text, "abstract")
-    keywords=extract_env(text, "keywords")
+    text = open(paper_file_name).read()
+    shortauthors = extract_shortauthors(text)
+    longauthors = extract_authors(text)
+    shorttitle = extract_shorttitle(text)
+    longtitle = extract_title(text)
+    abstract = extract_env(text, "abstract")
+    keywords = extract_env(text, "keywords")
     return shortauthors, longauthors, shorttitle, longtitle, abstract, keywords
 
+
 def remove_comments(tex_text):
-    out=''
+    out = ''
     print(tex_text.splitlines())
     for line in tex_text.splitlines():
-        new_line=line
-        pos=line.find("%")
+        new_line = line
+        pos = line.find("%")
         if pos > -1:
-            new_line=line[:pos]
+            new_line = line[:pos]
         out += new_line + '\n'
     return out.strip()
 
-def clear_title(title, is_short_title = False):
-    title=title.replace("%", "")
-    title=title.replace(r"\\", "")
-    title=title.replace("\n", "")
-    title=title.replace("\break", "")
-    title=title.replace("\centering", "")
-    title=re.sub(r"\s+", " ", title)
-    title=re.sub(r"\[.*?\] *", "", title)
-    title=re.sub(
+
+def clear_title(title, is_short_title=False):
+    title = title.replace("%", "")
+    title = title.replace(r"\\", "")
+    title = title.replace("\n", "")
+    title = title.replace("\break", "")
+    title = title.replace("\centering", "")
+    title = re.sub(r"\s+", " ", title)
+    title = re.sub(r"\[.*?\] *", "", title)
+    title = re.sub(
         r"\\(textnormal|vspace|small|large){.*?}", "", title)
-    title=re.sub(r"\\(systemname){.*?} :", "", title)
+    title = re.sub(r"\\(systemname){.*?} :", "", title)
     if is_short_title:
-        title=re.sub(r"---.*$", "", title)
-    title=re.sub(r"\\large.*$", "", title)
-    title=title.strip()
+        title = re.sub(r"---.*$", "", title)
+    title = re.sub(r"\\large.*$", "", title)
+    title = title.strip()
     return title
 
+
 def texify(text):
-    tex_replacement_map={'é': "\\'e", 'ó': "\\'{o}", 'ć': "\\'{c}", 'Ä': '\\"A', 'Ö': '\\"O', 'Ü': '\\"U',
+    tex_replacement_map = {'é': "\\'e", 'ó': "\\'{o}", 'ć': "\\'{c}", 'Ä': '\\"A', 'Ö': '\\"O', 'Ü': '\\"U',
                            'ß': '{\\ss}', 'ä': '\\"a', 'ö': '\\"o', 'ü': '\\"u', 'ñ': '\\~{n}', 'ń': "\\'n",
                            '„': '\\glqq', '“': '\\grqq', '”': "''", '’': "'{}", ' - ': ' --- '}
     for char in tex_replacement_map:
